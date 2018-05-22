4b4b04393327b6f464ea711f09571d5fecd736dd
Łukasz Jaworski <luskus@o2.pl>
2018-05-22 17:33:42 +0200

    Junit librarry + first tests added

---
+++ src/test/java/StartTests.java
@@ -0,0 +1,20 @@
+import com.sun.istack.internal.Nullable;
+import gui.StartCanvas;
+import org.junit.Test;
+
+import static junit.framework.TestCase.*;
+import static org.junit.Assert.*;
+public class StartTests {
+
+    @Test
+    public void creatingClass() {
+
+        StartCanvas testerCanvas = new StartCanvas(); // MyClass is tested
+        Start testerStart = new Start();
+        // assert statements
+        /*assertNotNull("StartObject",tester);
+        assertEquals(0, tester.multiply(0, 10), "0 x 10 must be 0");
+        assertEquals(0, tester.multiply(0, 0), "0 x 0 must be 0");
+*/
+    }
+}
